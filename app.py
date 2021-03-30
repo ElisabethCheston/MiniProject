b76d503a37eda9307d138201b5f50bc18c0bfafe
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-03-30 08:30:16 +0000

    Add new file codes.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route("/")
+@app.route("/")  
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
